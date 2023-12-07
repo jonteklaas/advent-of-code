45ad36c88d2511bdaf14cf2ca93e0e72d54646e5
Jonte Klaas <kontakt@jonteklaas.de>
2023-12-07 19:29:28 +0100

    Loese Aufgabe2 Tag06 2023

--- src/jahr2023/Tag06.java
+++ src/jahr2023/Tag06.java
@@ -9,6 +9,7 @@ public class Tag06
 	{
 		System.out.println(
 			new Tag06().loeseAufgabe1(new int[] { 35, 69, 68, 87 }, new int[] { 213, 1168, 1086, 1248 }));
+		System.out.println(new Tag06().loeseAufgabe2(35696887, 213116810861248L));
 	}
 
 	int loeseAufgabe1(int[] zeiten, int[] streckenRekorde)
@@ -17,25 +18,29 @@ public class Tag06
 		for (int i = 0; i < zeiten.length; i++)
 		{
 			produktDerAnzahlMoeglicherRekorde *=
-				ermittleAnzahlMoeglicheRekorde(ermittleMoeglicheStrecken(zeiten[i]), streckenRekorde[i]);
+				ermittleAnzahlMoeglicheRekorde(ermittleMoeglicheStrecken(zeiten[i]), (long) streckenRekorde[i]);
 		}
 		return produktDerAnzahlMoeglicherRekorde;
 	}
 
-	List<Integer> ermittleMoeglicheStrecken(int zeit)
+	int loeseAufgabe2(int zeit, Long strecke){
+		return ermittleAnzahlMoeglicheRekorde(ermittleMoeglicheStrecken(zeit), strecke);
+	}
+
+	List<Long> ermittleMoeglicheStrecken(int zeit)
 	{
-		List<Integer> strecken = new ArrayList<>();
-		for (int i = 0; i <= zeit; i++)
+		List<Long> strecken = new ArrayList<>();
+		for (long i = 0L; i <= zeit; i++)
 		{
-			strecken.add((zeit - i) * i);
+			strecken.add(((zeit - i) * i));
 		}
 		return strecken;
 	}
 
-	int ermittleAnzahlMoeglicheRekorde(List<Integer> moeglicheStrecken, int aktuellerRekord)
+	int ermittleAnzahlMoeglicheRekorde(List<Long> moeglicheStrecken, Long aktuellerRekord)
 	{
 		int anzahlMoeglicheRekorde = 0;
-		for (int moeglicheStrecke : moeglicheStrecken)
+		for (long moeglicheStrecke : moeglicheStrecken)
 		{
 			if (moeglicheStrecke > aktuellerRekord)
 			{
