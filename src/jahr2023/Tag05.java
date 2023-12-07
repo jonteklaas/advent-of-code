efffa8f2ef01b445f7328d2385151efb1c932868
Jonte Klaas <jonte.klaas@gmx.de>
2023-12-07 18:12:49 +0100

    Loese Aufgabe1 Tag05 2023

---
+++ src/jahr2023/Tag05.java
@@ -0,0 +1,92 @@
+package jahr2023;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Tag05
+{
+	Scanner scanner = new Scanner(new FileReader("src/jahr2023/resources/Tag05.txt"));
+
+	List<String> eingabe = new ArrayList<>();
+	List<String> aktuelleMap = new ArrayList<>();
+	List<Long> samen = new ArrayList<>();
+
+	List<Long> werte = new ArrayList<>();
+
+	public Tag05() throws FileNotFoundException
+	{}
+
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		new Tag05().berechneSumme1();
+	}
+
+	void berechneSumme1()
+	{
+		while (scanner.hasNext())
+		{
+			eingabe.add(scanner.nextLine());
+		}
+
+		String samenliste = eingabe.get(0).split(": ")[1];
+		for (int i = 0; i < samenliste.split(" ").length; i++)
+		{
+			//samen.add(Long.valueOf(samenliste.split(" ")[i]));
+			werte.add(Long.valueOf(samenliste.split(" ")[i]));
+		}
+
+		eingabe.remove(0);
+		int index = 0;
+
+		for (int i = 0; i < 7; i++)
+		{
+			aktuelleMap.clear();
+			index += 2;
+			while (index < eingabe.size() && !eingabe.get(index).isEmpty())
+			{
+				aktuelleMap.add(eingabe.get(index));
+				index++;
+			}
+			for (int j = 0; j < werte.size(); j++)
+			{
+				werte.set(j, loeseMapAuf(wandleMapInLongUm(aktuelleMap), werte.get(j)));
+			}
+		}
+		System.out.println("Ergebnis 1: " + Collections.min(werte));
+	}
+
+	List<List<Long>> wandleMapInLongUm(List<String> stringMap)
+	{
+		String[] zeile;
+		List<List<Long>> longMap = new ArrayList<>();
+		for (int i = 0; i < stringMap.size(); i++)
+		{
+			longMap.add(new ArrayList<>());
+			longMap.get(i).add(-1L);
+			longMap.get(i).add(-1L);
+			longMap.get(i).add(-1L);
+			zeile = stringMap.get(i).split(" ");
+			longMap.get(i).set(0, Long.valueOf(zeile[0]));
+			longMap.get(i).set(1, Long.valueOf(zeile[1]));
+			longMap.get(i).set(2, Long.valueOf(zeile[2]));
+		}
+		return longMap;
+	}
+
+	Long loeseMapAuf(List<List<Long>> longMap, Long quelle)
+	{
+		for (int i = 0; i < longMap.size(); i++) //ziel, quelle, range
+		{
+			Long differenz = quelle - longMap.get(i).get(1);
+			if (differenz >= 0 && differenz < longMap.get(i).get(2))
+			{
+				return longMap.get(i).get(0) + differenz;
+			}
+		}
+		return quelle;
+	}
+}
