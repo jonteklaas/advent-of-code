d95f5577c03e5038d90786e850c6629b4dd7ae71
Jonte Klaas <jonte.klaas@gmx.de>
2023-12-08 10:53:00 +0100

    Loese Aufgabe1 Tag07 2023

--- src/jahr2023/Tag07.java
+++ src/jahr2023/Tag07.java
@@ -9,18 +9,7 @@ public class Tag07
 	Scanner scanner = new Scanner(new FileReader("src/jahr2023/resources/Tag07.txt"));
 
 	List<String> eingabe = new ArrayList<>();
-	List<String> haende = new ArrayList<>();
-
-	List<List<Integer>> kartenwerte = new ArrayList<>();
-
-	List<List<Integer>> indizesTypen = new ArrayList<>();
-	static final int FUENF_GLEICHE = 6;
-	static final int VIER_GLEICHE = 5;
-	static final int FULL_HOUSE = 4;
-	static final int DREI_GLEICHE = 3;
-	static final int ZWEI_PAARE = 2;
-	static final int EIN_PAAR = 1;
-	static final int KEINE_GLEICHEN = 0;
+	List<List<Hand>> haende = new ArrayList<>();
 
 	public Tag07() throws FileNotFoundException
 	{
@@ -37,171 +26,67 @@ public class Tag07
 
 	int loeseAufgabe1()
 	{
-		int summe = 0;
-		indizesTypen.add(new ArrayList<>());
-		indizesTypen.add(new ArrayList<>());
-		indizesTypen.add(new ArrayList<>());
-		indizesTypen.add(new ArrayList<>());
-		indizesTypen.add(new ArrayList<>());
-		indizesTypen.add(new ArrayList<>());
-		indizesTypen.add(new ArrayList<>());
-		String[] zeile;
-		List<Integer> bids = new ArrayList<>();
+		int anzahlHaende = 0;
+		for (int i = 0; i < 7; i++)
+		{
+			haende.add(new ArrayList<>());
+		}
+
 		for (int i = 0; i < eingabe.size(); i++)
 		{
+			String[] zeile;
 			zeile = eingabe.get(i).split(" ");
-			haende.add(zeile[0]);
-			bids.add(Integer.parseInt(zeile[1]));
+			Hand aktuelleHand = new Hand(zeile[0], Integer.parseInt(zeile[1]));
+			haende.get(aktuelleHand.gibTyp()).add(aktuelleHand);
+			anzahlHaende++;
 		}
-		for (int i = 0; i < haende.size(); i++)
-		{
-			kartenwerte.add(Collections.singletonList(i));
-			kartenwerte.set(i, wandleHandInIntegerUm(haende.get(i)));
-		}
-		List<Integer> typenwerte = ermittleIndizesTypenwerte(haende);
 
-		int anzahlHaende = haende.size();
-		List<String> haendeZumRemoven = haende;
+		int summe = 0;
 		for(int i = 0; i < anzahlHaende; i++){
-
-			int summand = (haende.size() - i) * bids.get(ermittleIndexDerHoechstenVerbliebendenHand(typenwerte, haendeZumRemoven));
-			summe += summand;
+			Hand hoechsteHand = ermittleHoechsteVerbliebendeHand(haende);
+			System.out.println(haende.get(0).size());
+			summe += hoechsteHand.gibBid() * (anzahlHaende - i);
 		}
 		return summe;
 	}
 
-	private List<Integer> wandleHandInIntegerUm(String hand)
+	Hand ermittleHoechsteVerbliebendeHand(List<List<Hand>> verbleibendeHaende)
 	{
-		List<Integer> handAlsInteger = new ArrayList<>();
-		for (int i = 0; i < hand.length(); i++)
+		for (int i = 6; i >= 0; i--)
 		{
-			char aktuelleKarte = hand.charAt(i);
-			if (Character.isDigit(aktuelleKarte))
+			if (!verbleibendeHaende.get(i).isEmpty())
 			{
-				handAlsInteger.add(Integer.parseInt(Character.toString(aktuelleKarte)));
-			}
-			else
-			{
-
-				switch (aktuelleKarte)
-				{
-					case 'A':
-						handAlsInteger.add(14);
-					case 'K':
-						handAlsInteger.add(13);
-					case 'Q':
-						handAlsInteger.add(12);
-					case 'J':
-						handAlsInteger.add(11);
-					case 'T':
-						handAlsInteger.add(10);
-				}
-			}
-
-		}
-		return handAlsInteger;
-	}
-
-	int ermittleIndexDerHoechstenVerbliebendenHand(List<Integer> typenWerte, List<String> haendeZumRemoven)
-	{
-		for (int i = FUENF_GLEICHE; i >= 0; i--)
-		{
-			if (!indizesTypen.get(i).isEmpty())
-			{
-				return vergleicheHaende(indizesTypen.get(i), 0, haendeZumRemoven);
+				Hand aktuelleHand = vergleicheHaende(verbleibendeHaende.get(i), 0);
+				haende.get(i).remove(aktuelleHand);
+				return aktuelleHand;
 			}
 		}
-		return 1;
+		return null;
 	}
 
-	int vergleicheHaende(List<Integer> indizesTyp, int stelleZumVergleichen, List<String> haendeZumRemoven)
+	Hand vergleicheHaende(List<Hand> haende, int stelle)
 	{
 		int hoechsteHand = 0;
-		List<Integer> hoechsteHaende = new ArrayList<>();
-		for (int i = 0; i < indizesTyp.size(); i++)
+		List<Hand> hoechsteHaende = new ArrayList<>();
+
+		for (int i = 0; i < haende.size(); i++)
 		{
 			hoechsteHand =
-				Math.max(kartenwerte.get(indizesTyp.get(i)).get(stelleZumVergleichen), hoechsteHand);
+				Math.max(haende.get(i).gibWertDerKarte(stelle), hoechsteHand);
 		}
-		for (int i = 0; i < indizesTyp.size(); i++)
+
+		for (int i = 0; i < haende.size(); i++)
 		{
-			if (kartenwerte.get(indizesTyp.get(i)).get(stelleZumVergleichen) >= hoechsteHand)
+			if (haende.get(i).gibWertDerKarte(stelle) >= hoechsteHand)
 			{
-				hoechsteHaende.add(indizesTyp.get(i));
+				hoechsteHaende.add(haende.get(i));
 			}
 		}
 		if (hoechsteHaende.size() == 1)
 		{
-			kartenwerte.remove(kartenwerte.get(hoechsteHaende.get(0)));
 			return hoechsteHaende.get(0);
 		}
-		return vergleicheHaende(hoechsteHaende, stelleZumVergleichen + 1, haendeZumRemoven);
+		return vergleicheHaende(hoechsteHaende, stelle + 1);
 	}
 
-	List<Integer> ermittleIndizesTypenwerte(List<String> haende)
-	{
-		List<Integer> typenwerte = new ArrayList<>();
-		for (int i = 0; i < haende.size(); i++)
-		{
-			String hand = haende.get(i);
-			Set<Character> verschiedeneKarten = new HashSet<>();
-			for (int j = 0; j < hand.length(); j++)
-			{
-				verschiedeneKarten.add(hand.charAt(j));
-			}
-			int anzahlVerschiedeneKarten = verschiedeneKarten.size();
-			int anzahlMaximaleKarte = ermittleWieOftKartenInHandMaximalVorkommen(verschiedeneKarten, hand);
-			if (anzahlVerschiedeneKarten == 1)
-			{
-				typenwerte.add(FUENF_GLEICHE);
-				indizesTypen.get(FUENF_GLEICHE).add(i);
-			}
-			if (anzahlVerschiedeneKarten == 2)
-			{
-				if (anzahlMaximaleKarte == 4)
-				{
-					typenwerte.add(VIER_GLEICHE);
-					indizesTypen.get(VIER_GLEICHE).add(i);
-				}
-				typenwerte.add(FULL_HOUSE);
-				indizesTypen.get(FULL_HOUSE).add(i);
-			}
-			if (anzahlVerschiedeneKarten == 3)
-			{
-				if (anzahlMaximaleKarte == 3)
-				{
-					typenwerte.add(DREI_GLEICHE);
-					indizesTypen.get(DREI_GLEICHE).add(i);
-				}
-				typenwerte.add(ZWEI_PAARE);
-				indizesTypen.get(ZWEI_PAARE).add(i);
-			}
-			if (anzahlVerschiedeneKarten == 4)
-			{
-				typenwerte.add(EIN_PAAR);
-				indizesTypen.get(EIN_PAAR).add(i);
-			}
-			typenwerte.add(KEINE_GLEICHEN);
-			indizesTypen.get(KEINE_GLEICHEN).add(i);
-		}
-		return typenwerte;
-	}
-
-	int ermittleWieOftKartenInHandMaximalVorkommen(Set<Character> verschiedeneKarten, String hand)
-	{
-		int maximaleDopplungen = 1;
-		for (int i = 0; i < hand.length(); i++)
-		{
-			int dopplungen = 0;
-			for (Character karte : verschiedeneKarten)
-			{
-				if (karte.equals(hand.charAt(i)))
-				{
-					dopplungen++;
-				}
-			}
-			maximaleDopplungen = Math.max(dopplungen, maximaleDopplungen);
-		}
-		return maximaleDopplungen;
-	}
 }
